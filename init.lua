87d244342655c256f3e99f7f63025b481b063afb
Grzeszko <wyborskigrzesiek+web@gmail.com>
2025-02-04 01:23:37 +0100

    addrd linter and formatter

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number relativenumber")
+vim.cmd("set nowrap")
+
 require("config.lazy")
 require("config.keymaps")
