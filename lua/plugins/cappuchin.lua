76b0fd46f945c46a6ed1b08925a594faa627b85f
Grzeszko <wyborskigrzesiek+web@gmail.com>
2025-02-06 21:40:42 +0100

    code refactor and formating, added python to lsp

--- lua/plugins/cappuchin.lua
+++ lua/plugins/cappuchin.lua
@@ -1,11 +1,10 @@
-return{ 
-  {
-  "catppuccin/nvim", 
-  name = "catppuccin", 
-  priority = 1000,
-      config = function()
-      vim.cmd.colorscheme "catppuccin-mocha"
-    end
-  }
-
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ return {
     lazy = false,
     config = function()
       require("mason-lspconfig").setup({
-      ensure_installed = {"lua_ls","clangd"}
+      ensure_installed = {"lua_ls","clangd","pyright"}
       })
     end
   },
@@ -21,6 +21,7 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.clangd.setup({})
+      lspconfig.pyright.setup({})
       vim.keymap.set('n', 'K',vim.lsp.buf.hover,{})
       vim.keymap.set('n', '<leader>gd',vim.lsp.buf.definition,{})
       vim.keymap.set('n', '<leader>ca',vim.lsp.buf.code_action,{})
