b537899abe34d2d98c2e9f23c2af26d56fe424d8
Grzeszko <wyborskigrzesiek+web@gmail.com>
2025-02-22 11:32:06 +0100

    added tmux nvim navigator, updated o key behaviour to not enter insert mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 
+vim.api.nvim_set_keymap('n', 'o', 'mzO<Esc>`z', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'O', 'mzo<Esc>`z', { noremap = true, silent = true })
+
