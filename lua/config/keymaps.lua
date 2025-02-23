877300de2877217ea5411a76890653e749c6a43e
Grzeszko <wyborskigrzesiek+web@gmail.com>
2025-02-23 12:51:06 +0100

    also included autostart files and rest od nvim files bc git doing som bulshittery

--- lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-
-vim.api.nvim_set_keymap('n', 'o', 'mzO<Esc>`z', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'O', 'mzo<Esc>`z', { noremap = true, silent = true })
-
