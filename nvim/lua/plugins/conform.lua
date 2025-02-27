69fca318eb43c77bc549b2f263289c518c4174c7
Grzeszko <wyborskigrzesiek+web@gmail.com>
2025-02-27 16:51:09 +0100

    Adde clang-format, updated conform.lua
    Clang-format was removed from Mason and installed separatly to simplify
    config IMAO

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,22 +1,51 @@
-return{
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local conform = require("conform")
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
 
-      conform.setup({
-        formatters_by_ft = {
-          lua = { "stylua" },
-          cpp = { "clang-format"}
-        },
-      })
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("conform").format({ lsp_fallback = true, async = false, quiet = false, timeout_ms = 3000 })
+			end,
+			mode = { "n", "v" },
+			desc = "Format Injected Langs",
+		},
+	},
 
-      vim.keymap.set({ "n", "v" }, "<leader>ff", function()
-        conform.format({
-          lsp_fallback = true,
-          async = false,
-          timeout_ms = 1000,
-        })
-      end, { desc = "Format file or range (in visual mode)" })
-    end,
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			cpp = { "clang-format" },
+		},
+
+		format_on_save = {
+			lsp_format = "fallback",
+			timeout_ms = 500,
+		},
+
+		formatters = {
+			clang_format = {
+				-- Change where to find the command
+				command = "/usr/bin/clang-format",
+				prepend_args = { "--style=file:~/dotfiles/clang_format" },
+			},
+		},
+	},
+
+	-- config = function(_, opts)
+	-- 	local conform = require("conform")
+	--
+	-- 	conform.setup({
+	-- 		formatters_by_ft = {
+	-- 			lua = { "stylua" },
+	-- 			cpp = { "clang-format" },
+	-- 		},
+	--
+	-- 		format_on_save = {
+	-- 			lsp_format = "fallback",
+	-- 			timeout_ms = 500,
+	-- 		},
+	-- 	})
+	-- end,
 }
