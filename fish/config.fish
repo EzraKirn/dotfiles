8271b83657842f84a3155c22b347cfd0bf6c0a0f
Grzeszko <wyborskigrzesiek+web@gmail.com>
2025-03-22 00:18:46 +0100

    Fish: Stastup changed to neofetch

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,21 @@
 if status is-interactive
-    # Check if inside tmux already
-    if not set -q TMUX
-        # Try to attach to an existing session, or create a new one named 'main'
-        if tmux attach-session -t main 2>/dev/null
-            # Attached successfully
-        else
-            tmux new-session -s main
-        end
-    end
+    # # Check if inside tmux already
+    # if not set -q TMUX
+    #     # Try to attach to an existing session, or create a new one named 'main'
+    #     if tmux attach-session -t main 2>/dev/null
+    #         # Attached successfully
+    #     else
+    #         tmux new-session -s main
+    #     end
+    # end
+  neofetch
 end
 
 set -gx TERM "tmux-256color"
 
 
 # Aliases
+alias vi="nvim" 
 alias clr="clear" 
 alias tls="tmux list-sessions" 
 
