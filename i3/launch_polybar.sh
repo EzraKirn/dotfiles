0105df954f3873f47b4c9e918c29259992899828
Grzeszko <wyborskigrzesiek+web@gmail.com>
2025-03-28 23:37:17 +0100

    I3: Added picom to autostart, reorganised keybinds, added script to kill existing polybars after restart

--- i3/launch_polybar.sh
+++ i3/launch_polybar.sh
@@ -1,7 +1,13 @@
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
-  done
-else
-  polybar --reload toph &
-fi
+#!/bin/bash
+
+# Terminate already running Polybar instances
+pkill -x polybar
+
+# Wait for the processes to shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar &
+
+echo "Polybar reloaded successfully!"
+
