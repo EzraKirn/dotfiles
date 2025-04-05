858312b72046bc8dfc451dc0db696f739abf456e
Grzeszko <wyborskigrzesiek+web@gmail.com>
2025-04-05 11:31:42 +0200

    Nvim: added LSP for bash and shell

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,53 +1,53 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				-- ensure_installed = { "lua_ls", "clangd", "pyright" },
-				ensure_installed = { "lua_ls", "pyright" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		-- enabled = false,
-		opts = {
-			servers = {
-				clangd = {
-					init_options = {
-						provideFormatter = false,
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.clangd.setup({
-				cmd = {
-					"clangd",
-					"--log=verbose",
-					"--enable-config",
-					"--clang-tidy",
-					"--fallback-style=none",
-				},
-			})
-			lspconfig.pyright.setup({})
-
-			-- keybinds
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>df", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>dc", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        -- ensure_installed = { "lua_ls", "clangd", "pyright", "bashls" },
+        ensure_installed = { "lua_ls", "pyright", "bashls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    -- enabled = false,
+    opts = {
+      servers = {
+        clangd = {
+          init_options = {
+            provideFormatter = false,
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({
+        cmd = {
+          "clangd",
+          "--log=verbose",
+          "--enable-config",
+          "--clang-tidy",
+          "--fallback-style=none",
+        },
+      })
+      lspconfig.pyright.setup({})
+      lspconfig.bashls.setup {}
+      -- keybinds
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>df", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>dc", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+    end,
+  },
 }
