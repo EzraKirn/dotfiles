750e36ec672740f333ac1bbc108f506df6fc0b79
Grzeszko <wyborskigrzesiek+web@gmail.com>
2025-04-06 17:21:42 +0200

    Nvim: vimtex setup for fuckin LaTex

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,39 +1,40 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
 
-	keys = {
-		{
-			"<leader>ff",
-			function()
-				require("conform").format({ lsp_fallback = true, async = false, quiet = false, timeout_ms = 3000 })
-			end,
-			mode = { "n", "v" },
-			desc = "Format Injected Langs",
-		},
-	},
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        require("conform").format({ lsp_fallback = true, async = false, quiet = false, timeout_ms = 3000 })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+  },
 
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			cpp = { "clang-format" },
-		},
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      cpp = { "clang-format" },
+    },
 
-		format_on_save = {
-			lsp_format = "fallback",
-			timeout_ms = 500,
-		},
+    format_on_save = {
+      lsp_format = "fallback",
+      timeout_ms = 500,
+    },
 
-		formatters = {
-			clang_format = {
-				-- Change where to find the command
-				inherit = false,
-				command = "/usr/bin/clang-format",
-				args = { "--style=file" },
-				-- args = { "--style=GNU" },
-				-- args = { "--style=file:/home/grzeszko/dotfiles/.clang_format" },
-				-- prepend_args = { "--style=file:/home/grzeszko/dotfiles/.clang_format" },
-			},
-		},
-	},
+    formatters = {
+      clang_format = {
+        -- Change where to find the command
+        inherit = false,
+        command = "/usr/bin/clang-format",
+        args = { "--style=file" },
+        -- args = { "--style=GNU" },
+        -- args = { "--style=file:/home/grzeszko/dotfiles/.clang_format" },
+        -- prepend_args = { "--style=file:/home/grzeszko/dotfiles/.clang_format" },
+      },
+    },
+  },
+
 }
--- nvim/lua/plugins/origami.lua
+++ nvim/lua/plugins/origami.lua
@@ -1,15 +1,17 @@
 return {
-	"chrisgrieser/nvim-origami",
-	event = "VeryLazy",
-	opts = {}, -- needed even when using default config
-	config = function()
-		require("origami").setup({
-			keepFoldsAcrossSessions = true,
-			pauseFoldsOnSearch = true,
-			setupFoldKeymaps = true,
+  "chrisgrieser/nvim-origami",
+  event = "VeryLazy",
+  opts = {}, -- needed even when using default config
+  config = function()
+    require("origami").setup({
+      -- keepFoldsAcrossSessions = true,
+      pauseFoldsOnSearch = true,
+      setupFoldKeymaps = true,
 
-			-- `h` key opens on first column, not at first non-blank character or before
-			hOnlyOpensOnFirstColumn = false,
-		})
-	end,
+      -- foldKeymaps = {
+      --   setup = true, -- modifies `h` and `l`
+      --   hOnlyOpensOnFirstColumn = false,
+      -- },
+    })
+  end,
 }
