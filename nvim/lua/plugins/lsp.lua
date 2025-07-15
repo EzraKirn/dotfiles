7923b267c90fc75fb0a371ba2b23f45c9545a2e2
EzraKirn <wyborskigrzesiek+web@gmail.com>
2025-07-16 00:29:40 +0200

    Nvim: Expanded support for code generation and cpp clangd

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,4 +1,5 @@
 return {
+  -- Mason: LSP/DAP/Linter installer
   {
     "williamboman/mason.nvim",
     lazy = false,
@@ -6,48 +7,100 @@ return {
       require("mason").setup()
     end,
   },
+
+  -- Mason LSPConfig: Bridges mason + lspconfig
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "bashls", "texlab" },
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "bashls",
+          "texlab",
+          "clangd",
+          "cmake",
+        },
       })
     end,
   },
+
+  -- Clangd Extensions: Adds extra features for C++ (AST, inlay hints, easy switch header/source)
   {
-    "neovim/nvim-lspconfig",
-    -- enabled = false,
-    opts = {
-      servers = {
-        clangd = {
+    "p00f/clangd_extensions.nvim",
+    lazy = false,
+    config = function()
+      require("clangd_extensions").setup({
+        server = {
+          cmd = {
+            "clangd",
+            "--log=verbose",
+            "--enable-config",
+            "--clang-tidy",
+            "--fallback-style=none",
+          },
           init_options = {
             provideFormatter = false,
           },
         },
+      })
+      vim.keymap.set("n", "<leader>sh", "<cmd>ClangdSwitchSourceHeader<CR>")
+      vim.keymap.set("n", "<leader>ast", "<cmd>ClangdAST<CR>")
+      vim.keymap.set("n", "<leader>th", "<cmd>ClangdTypeHierarchy<CR>")
+      vim.keymap.set("n", "<leader>si", "<cmd>ClangdSymbolInfo<CR>")
+      vim.keymap.set("n", "<leader>mu", "<cmd>ClangdMemoryUsage<CR>")
+      vim.keymap.set("n", "<leader>ih", "<cmd>ClangdInlayHintsToggle<CR>")
+    end,
+  },
+
+  -- generate.nvim: C++ declaration/definition generator
+  {
+    "eriks47/generate.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    lazy = false,
+    config = function()
+      require("generate").setup()
+      vim.keymap.set("n", "<leader>gdf", ":Generate implementations<CR>", { desc = "Generate C++ implementations" })
+      vim.keymap.set("n", "<leader>gdc", ":Generate declarations<CR>", { desc = "Generate C++ declarations" })
+    end,
+  },
+
+  -- Core Neovim LSPConfig setup
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lua_ls = {},
+        pyright = {},
+        bashls = {},
+        texlab = {},
+        cmake = {},
       },
     },
     config = function(_, opts)
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({
-        cmd = {
-          "clangd",
-          "--log=verbose",
-          "--enable-config",
-          "--clang-tidy",
-          "--fallback-style=none",
-        },
-      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>df", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>dc", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+
+      -- Language servers
+      lspconfig.lua_ls.setup({ on_attach = on_attach })
+      lspconfig.pyright.setup({ on_attach = on_attach })
+      lspconfig.bashls.setup({ on_attach = on_attach })
       lspconfig.cmake.setup({
         on_attach = function(client, bufnr)
           client.server_capabilities.documentFormattingProvider = false
           client.server_capabilities.documentRangeFormattingProvider = false
+          on_attach(client, bufnr)
         end,
       })
-      lspconfig.pyright.setup({})
-      lspconfig.bashls.setup {}
-      lspconfig.texlab.setup {
+
+      lspconfig.texlab.setup({
+        on_attach = on_attach,
         settings = {
           texlab = {
             rootDirectory = nil,
@@ -63,7 +116,7 @@ return {
               }
             },
             diagnostics = {
-              ignoredPatterns = {}, -- optional
+              ignoredPatterns = {},
               includePaths = {
                 "/home/grzeszko/texmf/tex/latex/mylatex/LaTexPWR"
               }
@@ -76,24 +129,9 @@ return {
               onOpenAndSave = false,
               onEdit = false,
             },
-            -- diagnosticsDelay = 300,
-            -- latexFormatter = 'latexindent',
-            -- latexindent = {
-            --   ['local'] = nil, -- local is a reserved keyword
-            --   modifyLineBreaks = false,
-            -- },
-            -- bibtexFormatter = 'texlab',
-            -- formatterLineLength = 80,
           },
         },
-      }
-      -- lspconfig.cmake.setup {}
-      -- keybinds
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>df", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>dc", vim.lsp.buf.declaration, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      })
     end,
   },
 }
