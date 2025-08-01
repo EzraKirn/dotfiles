cab03304c7dec766a3a8bf2769319811544ceff3
EzraKirn <wyborskigrzesiek+web@gmail.com>
2025-08-02 00:13:40 +0200

    Nvim: Niclely working cpp debuger (Probably will b expanded in future)

--- nvim/lua/plugins/debugger/dap.lua
+++ nvim/lua/plugins/debugger/dap.lua
@@ -1,58 +1,62 @@
 return {
-	"mfussenegger/nvim-dap",
+  "mfussenegger/nvim-dap",
 
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		"theHamsta/nvim-dap-virtual-text",
-		"julianolf/nvim-dap-lldb",
-	},
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "theHamsta/nvim-dap-virtual-text",
+    -- "julianolf/nvim-dap-lldb", -- Optional: Only if you want helper templates (not essential)
+  },
 
-	opts = {},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
 
-		-- dapui setup
-		dapui.setup()
+    dapui.setup()
 
-		-- codelldb config
-		dap.adapters.codelldb = {
-			type = "server",
-			port = "${port}",
-			executable = {
-				command = "codelldb", -- or if not in $PATH: "/absolute/path/to/codelldb"
-				args = { "--port", "${port}" },
+    dap.adapters.codelldb = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = "codelldb", -- system path from yay
+        args = { "--port", "${port}" },
+        -- On Windows: uncomment 'detached = false'
+      },
+    }
 
-				-- On windows you may have to uncomment this:
-				-- detached = false,
-			},
-		}
+    -- C/C++/Rust debug configuration
+    for _, lang in ipairs({ "c", "cpp", "rust" }) do
+      dap.configurations[lang] = {
+        {
+          name = "Launch file",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false, -- Change to false if you prefer not to pause at main()
+          args = {},
+        },
+      }
+    end
 
-		-- DAP config
-		dap.configurations.cpp = {
-			{
-				name = "Launch file",
-				type = "codelldb",
-				request = "launch",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				cwd = "${workspaceFolder}",
-				stopOnEntry = false,
-			},
-		}
-		dap.configurations.c = dap.configurations.cpp
-		dap.configurations.rust = dap.configurations.cpp
+    vim.fn.sign_define('DapBreakpoint', {
+      text = '', -- Change this to your preferred symbol
+      texthl = 'DapBreakpoint',
+      linehl = '',
+      numhl = ''
+    })
+    vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = '#FF0000', bold = true })
 
-		-- Auto open/close dap-ui
-		dap.listeners.before.attach.dapui_config = function() dapui.open() end
-		dap.listeners.before.launch.dapui_config = function() dapui.open() end
-		dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
-		dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
+    -- DAP-UI auto open/close
+    dap.listeners.before.attach.dapui_config = function() dapui.open() end
+    dap.listeners.before.launch.dapui_config = function() dapui.open() end
+    dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+    dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
 
-		-- keybinds
-		-- vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
-		-- vim.keymap.set("n", "<leader>dc", dap.continue, {})
-	end,
+    -- Keymaps
+    vim.keymap.set("n", "<leader>d;", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<leader>db", dap.continue, {})
+  end,
 }
